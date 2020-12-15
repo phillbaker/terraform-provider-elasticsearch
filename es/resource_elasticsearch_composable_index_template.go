77a7fea64c61de2064e7591a2aec0c7aba9ff11a
Phillip Baker <phillbaker@retrodict.com>
2020-12-14 23:25:03 -0500

    fix linting

--- es/resource_elasticsearch_composable_index_template.go
+++ es/resource_elasticsearch_composable_index_template.go
@@ -9,8 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	elastic7 "github.com/olivere/elastic/v7"
-	elastic5 "gopkg.in/olivere/elastic.v5"
-	elastic6 "gopkg.in/olivere/elastic.v6"
 )
 
 func resourceElasticsearchComposableIndexTemplate() *schema.Resource {
