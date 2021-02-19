5dd3511e3bfe632e0fd72491bde2c517fd1542c1
Sergey Starostin <67504894+s-starostin@users.noreply.github.com>
2021-02-19 07:05:12 +0300

    Open Distro Kibana tenant resource (#144)
    * added opendistro kibana tenant resource for elastic v7 client
    
    * Updated formatting
    
    * fixed the resource test
    
    Co-authored-by: ЦЦТ-Старостин Сергей Юрьевич <sergey.starostin@tatar.ru>

---
+++ docs/resources/opendistro_kibana_tenant.md
@@ -0,0 +1,49 @@
+---
+layout: "elasticsearch"
+page_title: "Elasticsearch: elasticsearch_opendistro_kibana_tenant"
+subcategory: "Elasticsearch Open Distro"
+description: |-
+  Provides an Elasticsearch Open Distro Kibana tenant resource.
+---
+
+# elasticsearch_opendistro_kibana_tenant
+
+Provides an Elasticsearch Open Distro Kibana tenant resource.
+Please refer to the Open Distro [documentation][1] for details.
+
+## Example Usage
+
+```hcl
+# Create a tenant
+resource "elasticsearch_opendistro_kibana_tenant" "test" {
+  tenant_name   = "test"
+  description = "test tenant"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `tenant_name` -
+    (Required) The name of the tenant.
+* `description` -
+    (Optional) Description of the tenant.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` -
+    The name of the tenant.
+
+## Import
+
+Elasticsearch Open Distro tenant can be imported using the `tenant_name`, e.g.
+
+```
+$ terraform import elasticsearch_opendistro_kibana_tenant.writer test
+```
+
+<!-- External links -->
+[1]: https://opendistro.github.io/for-elasticsearch-docs/docs/security/access-control/multi-tenancy/
---
+++ es/resource_elasticsearch_opendistro_kibana_tenant_test.go
@@ -0,0 +1,157 @@
+package es
+
+import (
+	"fmt"
+	"testing"
+
+	elastic7 "github.com/olivere/elastic/v7"
+	elastic5 "gopkg.in/olivere/elastic.v5"
+	elastic6 "gopkg.in/olivere/elastic.v6"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccElasticsearchOpenDistroKibanaTenant(t *testing.T) {
+
+	provider := Provider().(*schema.Provider)
+	err := provider.Configure(&terraform.ResourceConfig{})
+	if err != nil {
+		t.Skipf("err: %s", err)
+	}
+	meta := provider.Meta()
+	esClient, err := getClient(meta.(*ProviderConf))
+	if err != nil {
+		t.Skipf("err: %s", err)
+	}
+	var allowed bool
+	switch esClient.(type) {
+	case *elastic5.Client:
+		allowed = false
+	case *elastic6.Client:
+		allowed = false
+	default:
+		allowed = true
+	}
+
+	randomName := "test" + acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			if !allowed {
+				t.Skip("Allowed only for ES >= 7")
+			}
+		},
+		Providers:    testAccOpendistroProviders,
+		CheckDestroy: testAccCheckElasticsearchOpenDistroKibanaTenantDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOpenDistroKibanaTenantResource(randomName),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckElasticSearchOpenDistroKibanaTenantExists("elasticsearch_opendistro_kibana_tenant.test"),
+					resource.TestCheckResourceAttr(
+						"elasticsearch_opendistro_kibana_tenant.test",
+						"id",
+						randomName,
+					),
+					resource.TestCheckResourceAttr(
+						"elasticsearch_opendistro_kibana_tenant.test",
+						"description",
+						"test",
+					),
+				),
+			},
+			{
+				Config: testAccOpenDistroKibanaTenantResourceUpdated(randomName),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckElasticSearchOpenDistroKibanaTenantExists("elasticsearch_opendistro_kibana_tenant.test"),
+					resource.TestCheckResourceAttr(
+						"elasticsearch_opendistro_kibana_tenant.test",
+						"description",
+						"test2",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckElasticsearchOpenDistroKibanaTenantDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "elasticsearch_opendistro_kibana_tenant" {
+			continue
+		}
+
+		meta := testAccOpendistroProvider.Meta()
+
+		var err error
+		esClient, err := getClient(meta.(*ProviderConf))
+		if err != nil {
+			return err
+		}
+		switch esClient.(type) {
+		case *elastic7.Client:
+			_, err = resourceElasticsearchGetOpenDistroKibanaTenant(rs.Primary.ID, meta.(*ProviderConf))
+		default:
+		}
+
+		if err != nil {
+			return nil // should be not found error
+		}
+
+		return fmt.Errorf("KibanaTenant %q still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
+func testCheckElasticSearchOpenDistroKibanaTenantExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "elasticsearch_opendistro_kibana_tenant" {
+				continue
+			}
+
+			meta := testAccOpendistroProvider.Meta()
+
+			var err error
+			esClient, err := getClient(meta.(*ProviderConf))
+			if err != nil {
+				return err
+			}
+			switch esClient.(type) {
+			case *elastic7.Client:
+				_, err = resourceElasticsearchGetOpenDistroKibanaTenant(rs.Primary.ID, meta.(*ProviderConf))
+			default:
+			}
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return nil
+	}
+}
+
+func testAccOpenDistroKibanaTenantResource(resourceName string) string {
+	return fmt.Sprintf(`
+	resource "elasticsearch_opendistro_kibana_tenant" "test" {
+		tenant_name = "%s"
+		description = "test"
+	}
+	`, resourceName)
+}
+
+func testAccOpenDistroKibanaTenantResourceUpdated(resourceName string) string {
+	return fmt.Sprintf(`
+	resource "elasticsearch_opendistro_kibana_tenant" "test" {
+		tenant_name = "%s"
+		description = "test2"
+	}
+	`, resourceName)
+}
