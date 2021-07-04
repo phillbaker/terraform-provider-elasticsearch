122490f93c6fb9a6a966ed3a5b1448ae87450f98
Phillip Baker <phillbaker@retrodict.com>
2021-07-04 17:22:01 -0400

    [sdk] Add debugging hooks for v2 sdk

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/phillbaker/terraform-provider-elasticsearch/es"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: es.Provider,
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/providers/phillbaker/elasticsearch",
+			&plugin.ServeOpts{
+				ProviderFunc: es.Provider,
+			},
+		)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: es.Provider,
+		})
+	}
 }
