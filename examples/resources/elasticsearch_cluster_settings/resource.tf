9f67083c8df6a937b0006b5bd235ca307b372af5
Phillip Baker <phillbaker@retrodict.com>
2022-05-03 10:28:45 -0400

    [cluster settings] Fix example formatting

--- examples/resources/elasticsearch_cluster_settings/resource.tf
+++ examples/resources/elasticsearch_cluster_settings/resource.tf
@@ -1,4 +1,4 @@
 resource "elasticsearch_cluster_settings" "global" {
   cluster_max_shards_per_node = 10
-  action_auto_create_index = "my-index-000001,index10,-index1*,+ind*"
+  action_auto_create_index    = "my-index-000001,index10,-index1*,+ind*"
 }
